77d7c839249fcf3ba499d5812f4bab2aa03dba75
ibelieve-mm <ibelieve1210@163.co>
2016-12-05 18:38:25 +0800

    获取到登陆信息

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,3 +1,8 @@
 Page({
-    
+    loginSubmit: function (e) {
+        var eventObj = e.detail.value;
+        var account = eventObj.account;
+        var password = eventObj.password;
+        console.log('账户：' + account + '\t密码：' + password, e.detail.value)
+    }
 })
